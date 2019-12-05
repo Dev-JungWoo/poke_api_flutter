798eed91811a6d9020ae51f75eeea76f9def9b0e
Jung Eun Woo <jung.woo@fluxfederation.com>
2019-12-06 09:31:38 +1300

    Show the pokemon list using ListView.

--- lib/pages/page_pokemon_list.dart
+++ lib/pages/page_pokemon_list.dart
@@ -22,6 +22,26 @@ class PokemonListPage extends StatelessWidget {
   }
 
   Widget buildListView(PokemonListModel data) {
+    return Scaffold(body: getListView(data));
+  }
+
+  Widget getListView(PokemonListModel data) {
+    return ListView.separated(
+      separatorBuilder: (context, index) => Divider(
+        color: Colors.black,
+      ),
+      itemCount: data.results.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('${data.results[index].name}'),
+          onTap: () {
+          },
+        );
+      },
+    );
+  }
+
+  Widget getGridView(PokemonListModel data) {
     return GridView.builder(
         itemCount: data.results.length,
         gridDelegate:
@@ -34,7 +54,7 @@ class PokemonListPage extends StatelessWidget {
                       Text(
                           data.results[index].name,
                           style: TextStyle(fontSize: 20)),
-                      //TODO: Load Image here.
+                      //TODO: Load Image here?
                     ]
                 ),
               )
